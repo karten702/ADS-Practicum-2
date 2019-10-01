0d31d05ca3bd14bfa366ce713d1c5f40a366a5fe
Martijn <karten702@gmail.com>
2019-10-01 12:30:54 +0200

    Arhcer Changes New path for registerScoreForRound to support setting a specific round's score and recalculate the scores for an archer. Added recalculateScores method to support the setting of a specific round and maintain the correct scores. Added clearScores to be able to test setting specific scores. Added setters for totalScore and weighted score to support a possible requirement of setting scores (see assignment manual v2)
    Added tests to see if the new methods work correctly.

--- src/test/java/nl/hva/ict/se/ads/ExtendedArcherTest.java
+++ src/test/java/nl/hva/ict/se/ads/ExtendedArcherTest.java
@@ -2,6 +2,7 @@ package nl.hva.ict.se.ads;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -28,5 +29,66 @@ public class ExtendedArcherTest extends ArcherTest {
         List<Archer> archers = Archer.generateArchers(1);
         assertEquals(-7, archers.get(0).weighted(0));
         assertEquals(4, archers.get(0).weighted(3));
+    }
+
+    @Test
+    public void archerResetScore(){
+        List<Archer> archers = Archer.generateArchers(1);
+        archers.get(0).clearScores();
+        assertEquals(0, archers.get(0).getTotalScore());
+        assertEquals(0, archers.get(0).getWeightedScore());
+    }
+
+    @Test
+    public void archerSetScore(){
+        List<Archer> archers = Archer.generateArchers(1);
+        Archer archer = archers.get(0);
+        int oldScore = archer.getTotalScore();
+        int oldWeighted = archer.getWeightedScore();
+        archer.setTotalScore(501);
+        archer.setWeightedScore(605);
+        assertTrue(oldScore != archer.getTotalScore());
+        assertTrue(oldWeighted != archer.getWeightedScore());
+        assertEquals(501, archer.getTotalScore());
+        assertEquals(605, archer.getWeightedScore());
+    }
+
+    @Test
+    public void archerCorrectScores(){
+        List<Archer> archers = Archer.generateArchers(1);
+        Archer archer = archers.get(0);
+        archer.clearScores();
+        archerFillPremadeScores(archer);
+        assertEquals(55, archer.getTotalScore());
+        assertEquals(51, archer.getWeightedScore());
+    }
+
+    @Test
+    public void archerCorrectChangedScore(){
+        List<Archer> archers = Archer.generateArchers(1);
+        Archer archer = archers.get(0);
+        archer.clearScores();
+        archerFillPremadeScores(archer);
+        archer.registerScoreForRound(2, new int[] {5,0,0});
+        assertEquals(38, archer.getTotalScore());
+        assertEquals(18, archer.getWeightedScore());
+    }
+
+    @Test
+    public void archerCorrectAddedRound(){
+        List<Archer> archers = Archer.generateArchers(1);
+        Archer archer = archers.get(0);
+        archer.clearScores();
+        archerFillPremadeScores(archer);
+        archer.registerScoreForRound(4, new int[] {5,0,0});
+        assertEquals(60, archer.getTotalScore());
+        assertEquals(43, archer.getWeightedScore());
+    }
+
+    public void archerFillPremadeScores(Archer archer){
+        archer.registerScoreForRound(0, new int[] {3,4,5});
+        archer.registerScoreForRound(1, new int[] {3,0,0});
+        archer.registerScoreForRound(2, new int[] {8,4,10});
+        archer.registerScoreForRound(3, new int[] {6,7,5});
     }
 }
